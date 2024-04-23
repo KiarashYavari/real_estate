8abb85e69157172bcf78e046e206504c413225a2
KiarashYavari <kiarash996@gmail.com>
2024-04-22 18:54:25 -0700

    create signin page basic form

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, Navigate } from 'react-router-dom';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../actions/auth';
 
-const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
+const SignIn = (login, isAuthenticated) => {
+  const [formData, setFormData] = useState({
+    email:'',
+    password:''
+  });
+  const {email, password} = formData;
+  const onChange = e => setFormData({...formData,[e.target.name]:e.target.value});
+
+  const onSubmit = e => {
+    e.preventDefault();
+    login(email, password);
+  };
+  if (isAuthenticated==true){
+    return <Navigate to='/' />
+  }
+
+  else {
+    return (
+      <div className='auth'>
+        <Helmet>
+          <title>Real Estate| login</title>
+          <meta name='description' content='real estate website login page' />
+        </Helmet>
+        <h1 className='auth__title'>Sign In</h1>
+        <p className='auth__lead'>sign in to your Account</p>
+        <form className='auth__form' onSubmit={e => onSubmit(e)}>
+          <div className='auth__form__group'>
+            <input className='auth__form__input' type='email' placeholder='email' name='email' value={email} onChange={e => onChange(e)} required />
+          </div>
+          <div className='auth__form__group'>
+            <input className='auth__form__input' type='password' placeholder='password' name='password' value={password} onChange={e => onChange(e)} minLength='6' />
+          </div>
+          <button className='auth__form__button'>Login</button>
+        </form>
+        <p className='auth__authtext'>
+          Don't have an account? <Link className='auth__authtext__link' to='/signup'>Sign Up</Link>
+        </p>
+
+      </div>
+
+  )}
 }
 
-export default SignIn
+SignIn.PropTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(SignIn);
