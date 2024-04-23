46ab56f0af57307456a2eaa2eebdf36307a1d92d
KiarashYavari <kiarash996@gmail.com>
2024-04-22 18:53:58 -0700

    setup react helmet provider

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SignIn from './containers/SignIn';
 import SignUp from './containers/SignUp';
 import NotFound from './components/NotFound';
 import Layout from './hocs/Layout';
+import { HelmetProvider } from 'react-helmet-async';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -17,22 +18,24 @@ import './sass/main.scss';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Layout>
-              <Routes>
-                  <Route exact path='/' element={<Home />} />
-                  <Route exact path='/about' element={<About />} />
-                  <Route exact path='/contact' element={<Contact />} />
-                  <Route exact path='/listings' element={<Listings />} />
-                  <Route exact path='/listings/:id' element={<ListingDetail />} />
-                  <Route exact path='/Signin' element={<SignIn />} />
-                  <Route exact path='/signup' element={<SignUp />} />
-                  <Route path='*' element={<NotFound />} />
-              </Routes>
-        </Layout>
-      </Router>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <Router>
+          <Layout>
+                <Routes>
+                    <Route exact path='/' element={<Home />} />
+                    <Route exact path='/about' element={<About />} />
+                    <Route exact path='/contact' element={<Contact />} />
+                    <Route exact path='/listings' element={<Listings />} />
+                    <Route exact path='/listings/:id' element={<ListingDetail />} />
+                    <Route exact path='/signin' element={<SignIn />} />
+                    <Route exact path='/signup' element={<SignUp />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+          </Layout>
+        </Router>
+      </Provider>
+    </HelmetProvider>
 
   )
 }
